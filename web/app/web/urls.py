1b05c900d428ec2e8418a4fdc07999ff5c5263d2
Thijs Coenen <info@thylacine.nl>
2017-12-13 10:44:39 +0100

    BAC-693 small clean-up

--- web/app/web/urls.py
+++ web/app/web/urls.py
@@ -4,7 +4,6 @@ from django.views.generic import TemplateView
 from .views import VestigingListView, VestigingDetailView
 
 urlpatterns = [
-    url(r'^demo/$', TemplateView.as_view(template_name='web/demo.html')),
     url(
         r'^vestiging/$',
         VestigingListView.as_view(template_name='web/vestiging_list.html')
