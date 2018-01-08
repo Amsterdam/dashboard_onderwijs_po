6b1b9c6c1d21756a7b225e33bb53bb6a2ca28158
Thijs Coenen <info@thylacine.nl>
2018-01-08 15:10:20 +0100

    BAC-623 Health check --- onderwijs tables not empty.

--- web/app/health/views.py
+++ web/app/health/views.py
@@ -4,6 +4,13 @@ from django.conf import settings
 from django.db import connection
 from django.http import HttpResponse
 
+from dataset.models import Vestiging
+from dataset.models import LeerlingNaarGewicht
+from dataset.models import SchoolType, SchoolAdvies
+from dataset.models import CitoScores
+from dataset.models import LeerlingLeraarRatio
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -38,6 +45,30 @@ def check_data(request):
     """
     Basic check for presence of data in the database.
     """
-    # TODO: Implement when models are available, raise HTTP 500 on problems.
+
+    # TODO: Add checks for priviliged data, once their handling is settled.
+    if not Vestiging.objects.count():
+        msg = 'There are no "Vestiging" entries.'
+        status = 500
+    elif not LeerlingNaarGewicht.objects.count():
+        msg = 'There are no "Leerling naar Gewicht" entries.'
+        status = 500
+    elif not SchoolAdvies.objects.count():
+        msg = 'There are no "School Advies" entries.'
+        status = 500
+    elif not SchoolType.objects.count():
+        msg = 'There are no "School Type" entries.'
+        status = 500
+    elif not CitoScores.objects.count():
+        msg = 'There are no "Cito Scores" entries.'
+        status = 500
+    elif not LeerlingLeraarRatio.objects.count():
+        msg = 'There are no "Leerling Leraar Ratio" entries.'
+        status = 500
+    else:
+        msg = 'Public data are available - basic database check passes. ' \
+              'No guarantee about referential integrity.'
+        status = 200
+
     return HttpResponse(
-        "Database data OK", content_type='text/plain', status=200)
+        msg, content_type='text/plain', status=status)
