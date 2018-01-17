c65f285bb7330f404bba76e206d8eb1fde7dc87c
Thijs Coenen <info@thylacine.nl>
2018-01-17 11:38:40 +0100

    support paginated API endpoints

--- web/app/web/static/web/draw.js
+++ web/app/web/static/web/draw.js
@@ -1,13 +1,43 @@
-var config = onderwijsConfig.generateConfig(brin6);
-vegaEmbed('#vis', config.specSchoolAdviezen, config.vegaEmbedOptions);
-vegaEmbed('#vis2', config.specLeerlingenNaarGewicht, config.vegaEmbedOptions);
+function paginatedJson(url, callback, ar){
+    // assumes .next property for next page
+    // assumes .next is null when no more pages available
+    // modifies ar in place
 
+    // make sure we have an array for our results:
+    if (ar === undefined){
+        var ar = [];
+    }
 
-// Subsidies (TODO: make this a proper table)
+    // download a page, check for following pages, download (recursively)
+    d3.json(url, function(error, data){
+        if (error) throw error;
 
-(function () {
+        ar = ar.concat(data.results);
+        if (data.next !== null) {
+            paginatedJson(data.next, callback, ar);
+        } else {
+            callback(null, ar);
+        };
+    });
+}
+
+function paginatedVegaEmbed(id, vegaSpec, vegaEmbedOptions){
+    // this modifies the provided vegaEmbedOptions !
+    var dataUrl = vegaSpec.data.url;
+
+    paginatedJson(dataUrl, function(error, data){
+        delete vegaSpec.data;
+        vegaSpec.data = {
+            'values': data
+        };
+        vegaEmbed(id, vegaSpec, vegaEmbedOptions);
+    });
+}
+
+function drawSubsidieTable(){
+    // TODO: make year filter configurable
     var subdidieDataUrl = "/onderwijs/api/toegewezen-subsidie/?vestiging=" + brin6 + "&subsidie__jaar=2017";
-    d3.json(subdidieDataUrl, function(error, subsidieData){
+    paginatedJson(subdidieDataUrl, function(error, subsidieData){
         var tmp = d3.select('#vis3')
         tmp.selectAll('div')
             .data(subsidieData).enter()
@@ -15,4 +45,9 @@ vegaEmbed('#vis2', config.specLeerlingenNaarGewicht, config.vegaEmbedOptions);
             .style('font-size', '12px')
             .text(function(d, i){ return d.subsidie + " " + d.aantal; });
     });
-})();
+}
+
+var config = onderwijsConfig.generateConfig(brin6);
+vegaEmbed('#vis', config.specSchoolAdviezen, config.vegaEmbedOptions);
+paginatedVegaEmbed('#vis2', config.specLeerlingenNaarGewicht, config.vegaEmbedOptions);
+drawSubsidieTable();
