d99c4b9476b759fae5f67751b48705c56406f5ea
Thijs Coenen <info@thylacine.nl>
2018-01-18 10:39:52 +0100

    flake8

--- web/app/dataset/management/commands/calc_aggregates.py
+++ web/app/dataset/management/commands/calc_aggregates.py
@@ -4,7 +4,6 @@ from django.core.management.base import BaseCommand
 from django.db.models import Avg
 from dataset.models import LeerlingLeraarRatio
 from dataset.models import CitoScores
-from dataset.models import SchoolWisselaars
 
 _YEARS = [2011, 2012, 2013, 2014, 2015, 2016]
 
@@ -25,15 +24,15 @@ class Command(BaseCommand):
             # Update all city wide average Leerling-Leraar Ratio's
             llr_avg = (
                 LeerlingLeraarRatio.objects.all()
-                    .filter(jaar__exact=year)
-                    .exclude(n_onderwijzend__isnull=True)
-                    .exclude(n_onderwijzend__exact=0)
-                    .aggregate(Avg('n_onderwijzend'))
+                .filter(jaar__exact=year)
+                .exclude(n_onderwijzend__isnull=True)
+                .exclude(n_onderwijzend__exact=0)
+                .aggregate(Avg('n_onderwijzend'))
             )
             (
                 LeerlingLeraarRatio.objects.all()
-                    .filter(jaar__exact=year)
-                    .update(n_onderwijzend_avg=llr_avg['n_onderwijzend__avg'])
+                .filter(jaar__exact=year)
+                .update(n_onderwijzend_avg=llr_avg['n_onderwijzend__avg'])
             )
 
             self.stdout.write(
@@ -42,15 +41,15 @@ class Command(BaseCommand):
             # Update all city wide average CITO scores for this year
             cito_scores_avg = (
                 CitoScores.objects.all()
-                    .filter(jaar__exact=year)
-                    .exclude(cet_gem__isnull=True)
-                    .exclude(cet_gem__exact=0)
-                    .aggregate(Avg('cet_gem'))
+                .filter(jaar__exact=year)
+                .exclude(cet_gem__isnull=True)
+                .exclude(cet_gem__exact=0)
+                .aggregate(Avg('cet_gem'))
             )
             (
                 CitoScores.objects.all()
-                    .filter(jaar__exact=year)
-                    .update(cet_gem_avg=cito_scores_avg['cet_gem__avg'])
+                .filter(jaar__exact=year)
+                .update(cet_gem_avg=cito_scores_avg['cet_gem__avg'])
             )
 
             self.stdout.write('Gemiddelde CITO score: {}'.format(cito_scores_avg))
