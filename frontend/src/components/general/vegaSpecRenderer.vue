8a3ff83a1b3bc16998c93dd910dfbd105e769dcb
Thijs Coenen <info@thylacine.nl>
2018-03-12 11:59:20 +0100

    Show numbers using dutch decimal notation.

--- frontend/src/components/general/vegaSpecRenderer.vue
+++ frontend/src/components/general/vegaSpecRenderer.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-import vegaEmbed from 'vega-embed'
+import * as vegaEmbed from 'vega-embed'
 
 const vegaEmbedOptions = {
   'actions': {
@@ -27,12 +27,19 @@ export default {
   },
   methods: {
     renderSpec () {
-      // console.log('Rendering vega spec', this.data, this.vegaspec)
       if (this.data && this.data.length && this.vegaspec) {
         // we modify the bound vegaSpec (overwrite the data property), then render it
         this.vegaspec.data = {values: this.data}
         let element = this.$refs[this.chartRef]
-        vegaEmbed(element, this.vegaspec, vegaEmbedOptions)
+
+        // https://gist.github.com/domoritz/a5707e5d9430c173019583ea8bc5707a (by vega / vega-lite co-author)
+        vegaEmbed.vega.formatLocale({
+          decimal: ',',
+          thousands: '.',
+          grouping: [3],
+          currency: ['', '\u00a0€']
+        })
+        vegaEmbed.default(element, this.vegaspec, vegaEmbedOptions)
       }
     }
   },
